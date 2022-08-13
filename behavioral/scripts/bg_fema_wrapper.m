3d213411b069fbcb4a47b97af762ebd97777786c
Diana Smith <dmysmith@gmail.com>
2022-08-13 10:57:27 -0700

    update to account for T and H effects in FEMA

---
+++ behavioral/scripts/bg_fema_wrapper.m
@@ -0,0 +1,96 @@
+%% Run FEMA for random effects project
+%% Diana Smith
+%% April 2022
+
+% - add simulated phenotype
+
+% Note: all models adjust for age and sex.
+
+% Model 1: (will run in OpenMx and FEMA)
+% FAE Model, twins only at baseline, genetic relatedness assumed
+% Model 2: (will run in OpenMx and FEMA)
+% FAE Model, twins only at baseline, with GRM included.
+% Model 3: 
+% 3a: FAE Model, full sample at baseline, with GRM included for everyone.
+% 3b: FAE Model, full sample at baseline, with GRM included only within family.
+% Model 4:
+% FAE Model, full sample at baseline, with GRM within family, plus covariates (genetic PCs, parental education, income).
+% Model 5:
+% 5a: FATE Model, full sample at baseline, with GRM (will explain more variance than prev model) +/- fixed covariates
+% 5b: FATHE Model, full sample at baseline, with GRM (will explain more variance than prev model) +/- fixed covariates 
+% Model 6: 
+% FATSE Model, full sample at two timepoints, with GRM +/- covariates (maybe add address effect to this one?)
+
+% ADD CMIG tools directory to MATLAB path:
+addpath(genpath('/home/d9smith/github/cmig_tools_internal'));
+
+% Specify data release
+dataRelease = '4.0';
+
+% Path to store results
+outDir = '/space/syn50/1/data/ABCD/d9smith/random_effects/behavioral';
+
+% specify array of random effects
+% random_effects = {{'F','A','E'};{'F','S','E'};{'F','A','S','E'};{'F','A','D','S','E'}};
+% random_effects = {{'F','A','E'}}; % just testing for now... DS 2020-08-11
+random_effects = {{'F','A','E'};{'F','A','T','E'};{'F','A','T','H','E'}};
+
+% specify array of design 
+fname_design = '/home/d9smith/projects/random_effects/behavioral/designMat/designMat1_allcovs.txt'; % change back later
+% designmat_dir = '/home/d9smith/projects/random_effects/behavioral';
+% designmat_file = dir(sprintf('%s/designMat*.txt', designmat_dir));
+% designmat_file = {designmat_file.name}';
+% designmat_array = strcat(designmat_dir, '/', designmat_file);
+
+% specify imaging file (the outcome you are predicting)
+dirname_imaging = '/space/syn50/1/data/ABCD/d9smith/random_effects/behavioral/nih_tbx_longitudinal.txt';
+
+% Inputs to FEMA_wrapper.m
+atlasVersion = 'ABCD2_cor10';
+dirname_tabulated = fullfile('/space/amdale/1/tmp/ABCD_cache/abcd-sync/4.0/tabulated/released/'); %KNOWN ISSUE: breaks when using txt files following NDA release --> must use pre-release csv files
+fname_pihat = fullfile('/space/amdale/1/tmp/ABCD_cache/abcd-sync/4.0/genomics/ABCD_rel4.0_grm.mat');
+fname_addressID = fullfile('/home/sabad/requests/recent_addr_07182022.csv'); 
+fname_pregnancyID = fullfile('/home/sabad/requests/pregnancy_ID_07182022.csv');
+
+% Optional inputs for `FEMA_wrapper.m` depending on analysis
+contrasts=[]; % Contrasts relate to columns in design matrix e.g. [1 -1] will take the difference between cols 1 and 2 in your design matrix (X)
+ranknorm = 0; % Rank normalizes dependent variables (Y) (default = 0)
+nperms = 0; % Number of permutations - set to 0 for now
+mediation = 0; % If wanting to use outputs for a mediation analysis set mediation=1 - ensures same resampling scheme used for each model in fname_design
+PermType = 'wildbootstrap'; %Default resampling method is null wild-bootstrap - to run mediation analysis need to use non-null wild-bootstrap ('wildboostrap-nn')
+tfce = 0; % Columns in design matrix to loop over to calculate TFCE - selecting columns of interest improves efficiency - is `isempty(tfce_cols)` FEMA_wrapper will NOT run TFCE
+
+datatype='external'; % can use txt with columns of ANY data type (e.g. ROIs, behavior) - runs mass univaraite LME across every column
+
+% if nperms > 0 will need to add Anders' script DELETE THIS SECTION
+% if nperms > 0
+if 0
+    addpath(genpath('~dale/matlab/SSE_local'));
+    % outDir = sprintf('%s/%.0fperms', outDir, nperms);
+end
+
+%% loop through each design matrix
+for i = 1:numel(designmat_array)
+   fname_design = designmat_array(i);
+   %% loop through each set of random effects
+   for j = 1:numel(random_effects)     
+        % define dynamic inputs
+        fstem_imaging = sprintf('%s',random_effects{j}{:});
+        % dirname_out = strcat(outDir, '/',designmat_file{i}(1:end-4)); % when looping through design matrices
+        dirname_out = strcat(outDir, '/',fname_design(1:-4)); % when only using one design matrix
+        disp(dirname_out);
+        RandomEffects = random_effects{j};
+        disp(RandomEffects);
+        
+        % run FEMA
+        [fnames_out beta_hat beta_se zmat logpmat sig2tvec sig2mat beta_hat_perm beta_se_perm zmat_perm sig2tvec_perm sig2mat_perm logLikvec_perm inputs] = FEMA_wrapper(fstem_imaging, fname_design, dirname_out, dirname_tabulated, dirname_imaging, datatype,...
+        'ranknorm', ranknorm, 'contrasts', contrasts, 'RandomEffects', RandomEffects, 'pihat_file', fname_pihat, 'nperms', nperms, 'mediation',mediation,'PermType',PermType,'tfce',tfce,'preg_file',fname_pregnancyID,'address_file',fname_addressID);
+        
+        % save sig2mat_perm as output
+        save(fnames_out{:}, 'sig2mat_perm', '-append');
+        save(fnames_out{:}, 'logLikvec_perm', '-append');
+        %writematrix(sig2mat_perm, sprintf('%s_sig2mat_perm.csv',fnames_out{:}(1:end-4)))
+        writematrix(sig2mat,sprintf('%s/sig2mat_%s.csv',dirname_out,[random_effects{j}{:}]));
+    end
+end
+
