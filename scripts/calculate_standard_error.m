bdf9933923e057157afaef3d32e653380e1db6df
Diana Smith <dmysmith@gmail.com>
2024-05-02 06:02:25 -0700

    minor bug fix

--- scripts/calculate_standard_error.m
+++ scripts/calculate_standard_error.m
@@ -12,12 +12,6 @@ savedir = '/home/d9smith/projects/random_effects/plots/std_err';
 RandomEffects = {'F' 'A' 'S' 'E'};
 modality = {'thickness' 'area' 'sulc'};
 
-% specify limits for plot based on vertvals
-fmax = 0.02; % max limit for colorbar - max for thickness is 0.0169
-fmin = 0; % min limit of colorbar
-fmid = fmax/2; % middle of colorbar
-fvals = [fmin fmid fmax];
-clim = [fmin fmax]; % set colorbar limits
 legendPosition = 'south';
 ico = 3;
 
@@ -31,15 +25,46 @@ for m=1:length(modality)
     std_err = std(sig2mat_perm(:,:,2:end),[],3)/sqrt(size(sig2mat_perm(:,:,2:end),3));  
 
     for re=1:length(RandomEffects)
+        % save map of standard error
         vertvals = std_err(re,:);
         statname = sprintf('%s ~ %s (standard error)', modality{m}, RandomEffects{re});
-        savefile = sprintf('%s_std_err_%s', modality{m}, RandomEffects{re});
+        savefile = sprintf('%s_std_err_%s.png', modality{m}, RandomEffects{re});
         
-        if ~exist(savepath, 'dir')
-            mkdir(savepath)
+        if ~exist(savedir, 'dir')
+            mkdir(savedir)
         end
+
+        % specify limits for plot based on vertvals
+        fmax = 0.0222; % max limit for colorbar - max for thickness 0.0169; area .0222, sulc is .0208
+        fmin = 0; % min limit of colorbar
+        fmid = fmax/2; % middle of colorbar
+        fvals = [fmin fmid fmax];
+        clim = [fmin fmax]; % set colorbar limits
         
         FEMA_run_showSurf(vertvals, statname, fvals, clim, ...
         'colormap', fire, 'legendPosition', legendPosition, 'ico', ico, 'savepath', fullfile(savedir, savefile));
+    
+        % save thresholded map based on 95% CI
+        ci = prctile(sig2mat_perm(re,:,2:end),[2.5 97.5], 3);
+        mask = ci(:,:,1)>0;
+
+        vertvals = sig2mat(re,:) .* mask;
+        statname = sprintf('%s ~ %s (thresholded)', modality{m}, RandomEffects{re});
+        savefile = sprintf('%s_thresholded_%s.png', modality{m}, RandomEffects{re});
+
+        % specify limits for plot based on vertvals
+        fmax = 1; % max limit for colorbar - max for thickness is 0.0169
+        fmin = 0; % min limit of colorbar
+        fmid = 0.5; % middle of colorbar
+        fvals = [fmin fmid fmax];
+        clim = [fmin fmax]; % set colorbar limits
+
+        FEMA_run_showSurf(vertvals, statname, fvals, clim, ...
+        'colormap', fire, 'legendPosition', legendPosition, 'ico', ico, 'savepath', fullfile(savedir, savefile));
+    
+    
     end
+
+    
+
 end
